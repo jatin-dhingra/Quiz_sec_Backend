7a34fb9699d406c69d4ecd0e3689ab66b38ee01d
jatin-dhingra <jatindhingra.dhingra99@gmail.com>
2023-06-09 01:56:33 +0530

    new changes to backend for connection

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 const cors=require('cors');
 const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cors());
 
 main().catch(err => console.log(err));
 
@@ -16,8 +19,9 @@ const userData = new mongoose.Schema({
     name: String,
     email:String,
     phone:String,
-    token:String,
-    score:Number
+    admin:String,
+    score:Number,
+    attempt:Number
 });
 
 const User = mongoose.model('User', userData);
@@ -30,22 +34,103 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-
 app.post('/quiz',async(req,res)=>{
 
     let user=new User();
     user.name=req.body.name;
     user.email=req.body.email;
     user.phone=req.body.phone;
-    user.token=req.body.token;
+    user.admin=req.body.admin;
     user.score=req.body.score;
+    user.attempt=req.body.attempt;
 
     const docs=await user.save();
     console.log(docs)
     res.json(docs);
 })
 
+
+app.get('/admin',async(req,res)=>{
+    try{
+        const allUser=await User.find({});
+        res.send({status:"ok",data:allUser});
+    }
+    catch(e){
+        console.log(e);
+    }
+    
+})
+
+
+const loginSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+
+const loginCollection=mongoose.model("loginCollection",loginSchema)
+
+
+app.get('/emplogin',cors(),(req,res)=>{
+
+    console.log('hello from new emp');
+})
+
+app.post('/emplogin',async(req,res)=>{
+    const {email,password}=req.body;
+
+    try{
+        const check=await loginCollection.findOne({email:email,password:password});
+        console.log(check);
+        
+        if(check)
+        {
+            res.json("exist");
+        }
+        else{
+            res.json("notexist")
+        }
+    }
+    catch(e){
+        res.json("notexist")
+    }
+})
  
+
+
+
+
+app.post('/newemp',async(req,res)=>{
+    const {email,password}=req.body;
+
+    const data={
+        email:email,
+        password:password
+    }
+    console.log(data);
+    try{
+        const check=await loginCollection.findOne({email:email});
+        if(check)
+        {
+            res.json("exist");
+        }
+        else{
+            res.json('notexist')
+            await loginCollection.insertMany([data]);
+        }
+    }
+    catch(e){
+        res.json("notexist")
+    }
+})
+
+
 
 app.listen(8000,(req,res)=>{
     
